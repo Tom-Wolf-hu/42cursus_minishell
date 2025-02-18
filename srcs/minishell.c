80db75233b03b77fd62082d510793c842f4f2ad9
Tamas Farkas <tfarkas@2-C-1.42heilbronn.de>
2025-02-18 14:52:45 +0100

    The modifications in minishell.c has been updated.

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: tfarkas <tfarkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 12:15:14 by alex              #+#    #+#             */
-/*   Updated: 2025/01/18 14:21:00 by alex             ###   ########.fr       */
+/*   Updated: 2025/02/18 14:49:04 by tfarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,274 @@ void	sig_handler(int sig)
 	if (sig == SIGINT)
 	{
 		write(1, "\n", 1);
-        rl_on_new_line();
-        rl_replace_line("", 1);
-        rl_redisplay();
+		rl_on_new_line();
+		rl_replace_line("", 1);
+		rl_redisplay();
 	}
+	if (sig == SIGQUIT)
+	{
+		rl_replace_line("", 1);
+		rl_redisplay();
+	}
+}
+
+void	ft_getcwd(char *line)
+{
+	char	buffer[128];
+	size_t	size;
+
+	size = 128;
+	if (getcwd(buffer, size) == NULL)
+	{
+		perror("minishell: getcwd");
+		free(line);
+		exit(1);
+	}
+	printf("%s\n", buffer);
+}
+
+void	free_arr(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
+void	handle_cd(char *line)
+{
+	char	**arr;
+	char	*path;
+	int 	i;
+
+	i = 0;
+	arr = ft_split(line, ' ');
+	if (!arr)
+		return (free(line), exit(1));
+	while (arr[i])
+		i++;
+	if (i > 2)
+	{
+		printf("minishell: cd: too many arguments\n");
+		free_arr(arr);
+		return ;
+	}
+	if (i == 1)
+	{
+		path = getenv("HOME");
+		if (!path)
+		{
+			printf("minishell: cd: HOME not set\n");
+			free_arr(arr);
+			return ;
+		}
+	}
+	else
+		path = arr[1];
+	if (chdir(path) != 0)
+		perror("minishell: cd");
+	free_arr(arr);
+}
+
+void	mywrite(char *line)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(line);
+	if (line[i] == 34)
+		i++;
+	if (line[len - 1] == 34)
+		len--;
+	while (i < len)
+	{
+		write(1, &line[i], 1);
+		i++;
+	}
+}
+
+void	show_input(char **arr, int flag)
+{
+	int	i;
+	int len;
+
+	if (flag)
+		i = 2;
+	else
+		i = 1;
+	len = 0;
+	while (arr[len])
+		len++;
+	while (arr[i])
+	{
+		if (i == len - 1)
+		{
+			mywrite(arr[i]);
+			if (!flag)
+				write(1, "\n", 1);
+			return ;
+		}
+		mywrite(arr[i]);
+		write(1, " ", 1);
+		i++;
+	}
+}
+
+void	handle_echo(char *line)
+{
+	char	**arr;
+	int		i;
+
+	i = 0;
+	arr = ft_split(line, ' ');
+	if (!arr)
+		return (free(line), exit(1));
+	while (arr[i])
+		i++;
+	if (i == 1)
+		return (write(1, "\n", 1), free_arr(arr));
+	if (i == 2)
+	{
+		if (ft_strcmp(arr[1], "-n") == 0)
+			return (free_arr(arr));
+		else
+			return (mywrite(arr[1]), write(1, "\n", 1), (void)0);
+	}
+	if (i > 2)
+	{
+		if (ft_strcmp(arr[1], "-n") == 0)
+			return (show_input(arr, 1), free_arr(arr));
+		show_input(arr, 0);
+	}
+	free_arr(arr);
+}
+
+int ft_isspace(int c)
+{
+    return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r');
+}
+
+int	is_empty(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (!ft_isspace(line[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+
+void	print_env(void)
+{
+	int				i;
+	extern	char	**environ;
+
+	i = 0;
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
+
+void	execute_cmd(char *line)
+{
+	pid_t	pid;
+	int		status;
+	char	*args[] = {line, NULL};
+	char	*env[] = {NULL};
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execve("/usr/bin/ls", args, env);
+		perror("execve");
+		exit(127);
+	}
+	else if (pid > 0)
+		waitpid(pid, &status, 0);
+	else
+		perror("fork");
+}
+
+int	check_line(char *line, int i)
+{
+	int	len;
+
+	len = ft_strlen(line);
+	while (i < len)
+	{
+		if (!ft_isspace(line[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	handle_export(char *line)
+{
+	char	*arg;
+	char	*equals_pos;
+	char	*value;
+
+	if (ft_strlen(line) == 6 || check_line(line, 7))
+		print_env();
+	else
+	{
+		arg = line + 7;
+		equals_pos = ft_strchr(arg, '=');
+		if (equals_pos != NULL)
+		{
+			*equals_pos = '\0';
+			value = equals_pos + 1;
+			if (setenv(arg, value, 1) == -1)
+				perror("setenv");
+		}
+		else
+			printf("export: invalid syntax\n");
+	}
+}
+
+void	handle_unset(char *line)
+{
+	char	*arg;
+
+	arg = line + 6;
+	if (unsetenv(arg) == -1)
+		perror("unsetenv");
+}
+
+void	choose_cmd(char *line)
+{
+	if (is_empty(line))
+		return ;
+	if (ft_strcmp(line, "pwd") == 0)
+		ft_getcwd(line);
+	else if (ft_strncmp(line, "cd", 2) == 0)
+		handle_cd(line);
+	else if (ft_strncmp(line, "echo", 4) == 0)
+		handle_echo(line);
+	else if (ft_strcmp(line, "env") == 0)
+		print_env();
+	else if (ft_strncmp(line, "export", 6) == 0)
+		handle_export(line);
+	else if (ft_strncmp(line, "unset", 5) == 0)
+		handle_unset(line);
+	else
+		execute_cmd(line);
+	// else
+		// printf("minishell: command not found: %s\n", line);
 }
 
 int main(void)
@@ -36,6 +300,7 @@ int main(void)
 	while (1)
 	{
 		signal(SIGINT, sig_handler);
+		signal(SIGQUIT, sig_handler);
 		line = readline("> ");
 		if (!line || ft_strcmp(line, "exit") == 0)
 			break ;
@@ -44,13 +309,10 @@ int main(void)
 		else
 		{
 			add_history(line);
-			printf("You wrote: %s\n", line);
+			choose_cmd(line);
 		}
 		free(line);
 	}
 	rl_clear_history();
 	free(line);
-	printf("Bye!\n");
 }
-// ctrl+c завершить все дочерние процессы (но не саму оболочку)
-// ctrl+d 
