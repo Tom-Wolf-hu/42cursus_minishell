87d49927b9a9078a99567bb83493af93a1ba54c5
Oleksii Malovichko <omalovic@2-C-8.42heilbronn.de>
2025-02-26 15:19:12 +0100

    c

--- srcs/handle_export_unset.c
+++ srcs/handle_export_unset.c
@@ -6,7 +6,7 @@
 /*   By: omalovic <omalovic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 12:49:13 by omalovic          #+#    #+#             */
-/*   Updated: 2025/02/26 13:26:51 by omalovic         ###   ########.fr       */
+/*   Updated: 2025/02/26 15:14:53 by omalovic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ extern char **environ; // Массив переменных окружения
 
 int find_var_in_env(char *name)
 {
-    int i = 0;
+    int i;
 
+	i = 0;
     while (environ[i])
 	{
         if (ft_strncmp(environ[i], name, ft_strlen(name)) == 0 && environ[i][ft_strlen(name)] == '=')
@@ -27,51 +28,48 @@ int find_var_in_env(char *name)
     return (-1);
 }
 
-// Функция для установки переменной в окружении
 int mysetenv(char *name, char *value)
 {
-    int index = find_var_in_env(name);
-    char *new_var;
-    
-    // Если переменная уже существует, обновим её
-    if (index != -1)
+	int index;
+	char *new_var;
+
+	index = find_var_in_env(name);
+	if (index != -1)
 	{
-        free(environ[index]);  // Освободить старое значение
-        new_var = malloc(strlen(name) + strlen(value) + 2); // +2 для '=' и '\0'
-        if (new_var == NULL)
-            return (perror("malloc"), 1);
-        // Строку создаём вручную, без использования sprintf
-        strcpy(new_var, name); // Копируем имя переменной
-        strcat(new_var, "=");  // Добавляем знак '='
-        strcat(new_var, value); // Добавляем значение переменной
-        
-        environ[index] = new_var; // Обновляем переменную окружения
-    }
+		free(environ[index]);
+		new_var = malloc(strlen(name) + strlen(value) + 2);
+		if (new_var == NULL)
+			return (perror("malloc"), 1);
+		strcpy(new_var, name); // Копируем имя переменной
+		strcat(new_var, "=");  // Добавляем знак '='
+		strcat(new_var, value); // Добавляем значение переменной
+		environ[index] = new_var; // Обновляем переменную окружения
+	}
 	else
 	{
-        // Если переменной нет, добавим её в окружение
-        int count = 0;
-        while (environ[count]) count++; // Считаем количество переменных в окружении
-        environ = realloc(environ, sizeof(char*) * (count + 2)); // Выделяем место для новой переменной
-        if (environ == NULL) {
-            perror("realloc");
-            return (1);
-        }
-        
-        new_var = malloc(strlen(name) + strlen(value) + 2); // +2 для '=' и '\0'
-        if (new_var == NULL) {
-            perror("malloc");
-            return (1);
-        }
-        
-        // Строку создаём вручную
-        strcpy(new_var, name);  // Копируем имя переменной
-        strcat(new_var, "=");   // Добавляем знак '='
-        strcat(new_var, value); // Добавляем значение переменной
-        
-        environ[count] = new_var; // Добавляем в окружение
-        environ[count + 1] = NULL; // Обязательно завершаем массив NULL
-    }
+		// Если переменной нет, добавим её в окружение
+		int count = 0;
+		while (environ[count]) count++; // Считаем количество переменных в окружении
+		environ = realloc(environ, sizeof(char*) * (count + 2)); // Выделяем место для новой переменной
+		if (environ == NULL) {
+			perror("realloc");
+			return (1);
+		}
+		
+		new_var = malloc(strlen(name) + strlen(value) + 2); // +2 для '=' и '\0'
+		if (new_var == NULL) {
+			perror("malloc");
+			return (1);
+		}
+		
+		// Строку создаём вручную
+		strcpy(new_var, name);  // Копируем имя переменной
+		strcat(new_var, "=");   // Добавляем знак '='
+		strcat(new_var, value); // Добавляем значение переменной
+		
+		environ[count] = new_var; // Добавляем в окружение
+		environ[count + 1] = NULL; // Обязательно завершаем массив NULL
+	}
 	return (0);
 }
 
@@ -99,29 +97,26 @@ int	handle_export(char *line)
 	return (0);
 }
 
-// Функция для удаления переменной из окружения
 int my_unsetenv(char *name)
 {
-    int index = find_var_in_env(name);
-    
-    // Если переменная найдена
-    if (index != -1)
+	int index;
+
+	index = find_var_in_env(name);
+	if (index != -1)
 	{
-        free(environ[index]); // Освобождаем память, занятую переменной
-
-        // Сдвигаем все последующие элементы на одну позицию влево
-        while (environ[index]) {
-            environ[index] = environ[index + 1];
-            index++;
-        }
-
-        // Обновляем массив окружения, уменьшив его размер на 1
-        environ = realloc(environ, sizeof(char*) * (index)); 
-        if (environ == NULL && index > 0) {
-            perror("realloc");
-            return (1);
-        }
-    }
+		free(environ[index]);
+		while (environ[index])
+		{
+			environ[index] = environ[index + 1];
+			index++;
+		}
+		environ = realloc(environ, sizeof(char*) * (index)); 
+		if (environ == NULL && index > 0)
+		{
+			perror("realloc");
+			return (1);
+		}
+	}
 	return (0);
 }
 
