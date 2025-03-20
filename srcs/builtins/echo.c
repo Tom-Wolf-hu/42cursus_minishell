301dbb1b5de7722840582129ae9015b2333a060f
alex <alex@dev-kate-log>
2025-03-20 18:14:47 +0100

    solved the last problems with exec

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 12:40:45 by omalovic          #+#    #+#             */
-/*   Updated: 2025/03/17 16:10:07 by alex             ###   ########.fr       */
+/*   Updated: 2025/03/20 18:10:25 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,23 +61,23 @@ int	handle_echo(char *line, int fd)
 {
 	char	**arr;
 	int		i;
-	char	*line_to_check;
+	// char	*line_to_check;
 
-	line_to_check = ft_strdup(line);
+	// line_to_check = ft_strdup(line);
 	i = 0;
-	arr = ft_split(line_to_check, ' ');
+	arr = ft_split(line, ' ');
 	if (!arr)
-		return (free(line), free(line_to_check), exit(1), 1);
+		return (free(line), exit(1), 1);
 	while (arr[i])
 		i++;
 	if (i == 1)
 	{
-		return (write(fd, "\n", 1), free_arr(arr), free(line_to_check), 0);
+		return (write(fd, "\n", 1), free_arr(arr), 0);
 	}
 	if (i == 2)
 	{
 		if (ft_strcmp(arr[1], "-n") == 0)
-			return (free_arr(arr), free(line), free(line_to_check), 0);
+			return (free_arr(arr), 0);
 		else
 			return (mywrite(arr[1], fd), write(1, "\n", 1), 0);
 	}
